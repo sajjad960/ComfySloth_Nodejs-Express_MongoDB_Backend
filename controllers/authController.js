ad2f8ab8430925b24dd3a606eee69460c7f247b3
sajjad960 <sajjadhossainsb01@gmail.com>
2021-09-27 02:17:24 +0600

    working on resetPassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -121,7 +121,7 @@ exports.forgotPassword = async (req, res, next) => {
         await user.save({validateBeforeSave: false})
 
         // 3) Send it to user's email
-        const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`
+        const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`
 
         const message = `Forgot your password ? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password please ignore this message`;
 
@@ -149,3 +149,9 @@ exports.forgotPassword = async (req, res, next) => {
         next(err)
     }
 }
+
+
+exports.resetPassword = async (req, res, ) => {
+
+}
+
